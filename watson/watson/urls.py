fbd58eb7a6d00634f6918b8f55da76495f1b5f8e
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2018-08-21 18:17:34 -0700

    Work on serving media folder in development.

--- watson/watson/urls.py
+++ watson/watson/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 import accounts.routers
 import screenshots.routers
@@ -27,3 +29,8 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
